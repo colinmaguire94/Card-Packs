071171aca4324c8a08f40a98068689266396ccfa
Colin Maguire <colin.maguire.94@gmail.com>
2018-03-21 05:52:36 -0400

    Add files via upload

---
+++ Scripts/ImportCards.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class ImportCards {
+
+    //public string[] Basic = new string[10000];
+    //public string[] Common = new string[10000];
+    //public string[] Uncommon = new string[10000];
+    //public string[] Rare = new string[10000];
+    //public string[] Epic = new string[10000];
+
+    public string name;
+    public Cards cards;
+
+    public bool LoadCards()
+    {
+        cards = new Cards();
+
+        if (File.Exists(Application.dataPath + "\\Lootboxes\\cards.json"))
+        {
+            string jsonFile = File.ReadAllText(Application.dataPath + "\\Lootboxes\\cards.json");
+            cards = JsonUtility.FromJson<Cards>(jsonFile);
+            return true;
+        }
+        return false;
+    }
+}
+
+[System.Serializable]
+public struct Cards
+{
+    public CardDetails[] Basic;
+    public CardDetails[] Common;
+    public CardDetails[] Uncommon;
+    public CardDetails[] Rare;
+    public CardDetails[] Epic;
+}
+
+[System.Serializable]
+public struct CardDetails
+{
+    public int id;
+    public string cardArt;
+}
+//public class Cards
+//{
+//    public int[] Basic = new int[50];
+//    public int[] Common = new int[50];
+//    public int[] Uncommon = new int[50];
+//    public int[] Rare = new int[50];
+//    public int[] Epic = new int[50];
+//}
---
+++ Scripts/RandomCardManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCardManager : MonoBehaviour {
+
+    public enum rarity { NONE, BASIC, COMMON, UNCOMMON, RARE, EPIC };
+    ImportCards cards = new ImportCards();
+    public GameObject cardGO;
+
+
+    // Use this for initialization
+    IEnumerator Start () {
+        Debug.Log(cards.LoadCards());
+        for (int i = 0; i < 5; i++)
+        {
+            int rand = Random.Range(1, 1000);
+            if (rand <= 350)
+            {
+                int rand2 = Random.Range(0, cards.cards.Basic.Length - 1);
+                WWW www = new WWW(cards.cards.Basic[rand2].cardArt);
+                yield return www;
+                GameObject card = Instantiate(cardGO);
+                card.transform.position = new Vector3(card.transform.position.x + (1.0f * i), card.transform.position.y, card.transform.position.z + 3.0f);
+                card.GetComponent<Renderer>().material.mainTexture = www.texture;
+                Debug.Log("Basic");
+            }
+            else if (rand <= 600)
+            {
+                int rand2 = Random.Range(0, cards.cards.Common.Length - 1);
+                WWW www = new WWW(cards.cards.Common[rand2].cardArt);
+                yield return www;
+                GameObject card = Instantiate(cardGO);
+                card.transform.position = new Vector3(card.transform.position.x + (1.0f * i), card.transform.position.y, card.transform.position.z + 3.0f);
+                card.GetComponent<Renderer>().material.mainTexture = www.texture;
+                Debug.Log("Common");
+            }
+            else if (rand <= 800)
+            {
+                int rand2 = Random.Range(0, cards.cards.Uncommon.Length - 1);
+                WWW www = new WWW(cards.cards.Uncommon[rand2].cardArt);
+                yield return www;
+                GameObject card = Instantiate(cardGO);
+                card.transform.position = new Vector3(card.transform.position.x + (1.0f * i), card.transform.position.y, card.transform.position.z + 3.0f);
+                card.GetComponent<Renderer>().material.mainTexture = www.texture;
+                Debug.Log("Uncommon");
+            }
+            else if (rand <= 950)
+            {
+                int rand2 = Random.Range(0, cards.cards.Rare.Length - 1);
+                WWW www = new WWW(cards.cards.Rare[rand2].cardArt);
+                yield return www;
+                GameObject card = Instantiate(cardGO);
+                card.transform.position = new Vector3(card.transform.position.x + (1.0f * i), card.transform.position.y, card.transform.position.z + 3.0f);
+                
+                card.GetComponent<Renderer>().material.mainTexture = www.texture;
+                Debug.Log("Rare");
+            }
+            else
+            {
+                int rand2 = Random.Range(0, cards.cards.Epic.Length - 1);
+                WWW www = new WWW(cards.cards.Epic[rand2].cardArt);
+                yield return www;
+                GameObject card = Instantiate(cardGO);
+                card.transform.position = new Vector3(card.transform.position.x + (1.0f * i), card.transform.position.y, card.transform.position.z + 3.0f);
+                card.GetComponent<Renderer>().material.mainTexture = www.texture;
+                Debug.Log("Epic");
+            }
+        }
+        yield return null;
+    }
+
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
